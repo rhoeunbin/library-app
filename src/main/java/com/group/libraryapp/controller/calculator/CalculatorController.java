d295f5b92954ba3f8243f71fb5838bdb370fb206
rhoeunbin <rhoeb21@gmail.com>
2024-05-01 23:58:21 +0900

    [feat] POST API 사용해보기

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,9 +1,10 @@
 package com.group.libraryapp.controller.calculator;
 
-import com.group.libraryapp.dto.calculator.request.CalculatorAddRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.group.libraryapp.dto.calculator.request.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController // 1. 이 클래스를 API의 진입 지점으로 만들어줌
 public class CalculatorController {
@@ -11,5 +12,32 @@ public class CalculatorController {
     @GetMapping("/add") // 2. GET /add
     public int addTwoNumbers(CalculatorAddRequest request){
         return request.getNum1() + request.getNum2();
+    }
+
+    @PostMapping("/multiply") // POST /multiply
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        return request.getNum1() * request.getNum2();
+    }
+
+    // 과제2
+    @GetMapping("/api/v1/calc") // 2. GET /add
+    public CalculatorResponse calculator(CalculatorRequest request){
+        return new CalculatorResponse(request);
+    }
+
+    @GetMapping("/api/v1/day-of-the-week")
+    public DayResponse dayOfWeek(@RequestParam("date") String date){
+        LocalDate localDate = LocalDate.parse(date);
+        return new DayResponse(localDate);
+    }
+
+    @PostMapping("/api/v1/arr")
+    public int sum(@RequestBody ArrayRequest request) {
+        int res = 0;
+        for(int i = 0; i < request.getNumbers().size(); i++) {
+            res += request.getNumbers().get(i);
+        }
+
+        return res;
     }
 }
