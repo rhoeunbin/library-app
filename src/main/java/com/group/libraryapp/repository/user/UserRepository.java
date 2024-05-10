f2b1b20553a4ee2bbd97cbbaed9f0f4f34a5c8b1
rhoeunbin <rhoeb21@gmail.com>
2024-05-10 23:59:34 +0900

    [feat] UserController 기능 분리 -> UserService, UserRepository

---
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -0,0 +1,57 @@
+package com.group.libraryapp.repository.user;
+
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+// sql을 사용해 실제 db와의 통신 담당
+public class UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // 유저 생성
+    public void saveUser(String name, Integer age){
+        String sql = "INSERT INTO user(name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, name, age);
+    }
+
+    // 유저 조회
+    public List<UserResponse> getUsers() {
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
+    }
+
+    // 현재 유저가 있는지, 없는지 등을 확인하고 예외 처리 담당 => id로
+    public boolean isUserNotExist(long id){ // 1. 함수 만들기 -> 3, jdbcTemplate 연결 + id
+        String readSql = "SELECT * FROM user WHERE id = ?"; // 2. Service에서 db연결 코드 복붙해오기
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, id).isEmpty();
+    }
+
+    // 유저 이름 업데이트
+    public void updateUserName(String name, long id){
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, name, id);
+    }
+
+    // 현재 유저가 있는지, 없는지 등을 확인하고 예외 처리 담당 => name으로
+    public boolean isUserNotExist(String name) {
+        String readSql = "SELECT * FROM user WHERE name = ?";
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
+    }
+
+    // 유저 삭제
+    public void deleteUser(String name){
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
+    }
+}
---
+++ src/main/java/com/group/libraryapp/service/user/UserService.java
@@ -0,0 +1,47 @@
+package com.group.libraryapp.service.user;
+
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.repository.user.UserRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class UserService {
+    // repository 사용하기 위해 불러오기
+    private final UserRepository userRepository;
+
+    public UserService(JdbcTemplate jdbcTemplate) {
+        userRepository = new UserRepository(jdbcTemplate);
+    }
+
+    // 유저 생성
+    public void saveUser(UserCreateRequest request){
+        userRepository.saveUser(request.getName(), request.getAge());
+    }
+
+    // 유저 조회
+    public List<UserResponse> getUsers(){
+        return userRepository.getUsers();
+    }
+
+    // 유저 이름 업데이트
+    // 현재 유저가 있는지, 없는지 등을 확인하고 예외 처리 담당
+    public void updateUser(UserUpdateRequest request){
+        // 코드 간결하게 줄이기
+        if (userRepository.isUserNotExist(request.getId())) {
+            throw new IllegalArgumentException();
+        }
+
+        userRepository.updateUserName(request.getName(), request.getId());
+    }
+
+    // 유저 삭제
+    public void deleteUser(String name){
+        if (userRepository.isUserNotExist(name)) {
+            throw new IllegalArgumentException();
+        }
+        userRepository.deleteUser(name);
+    }
+}
