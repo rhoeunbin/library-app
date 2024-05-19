fc4b18b5d370b8eb4aac5cefaa5a18dcc46fcb72
rhoeunbin <rhoeb21@gmail.com>
2024-05-19 19:11:32 +0900

    [feat] create Book Entity

--- src/main/java/com/group/libraryapp/domain/book/Book.java
+++ src/main/java/com/group/libraryapp/domain/book/Book.java
@@ -11,4 +11,15 @@ public class Book {
 
     @Column(nullable = false)
     private String name;
+
+    protected Book(){
+
+    }
+
+    public Book(String name) {
+        if (name == null || name.isBlank()){
+            throw new IllegalArgumentException(String.format("잘못된 name(%s)이 들어왔습니다"));
+        }
+        this.name = name;
+    }
 }
