81738f008b3c0547360f6f23476efde4916347a3
rhoeunbin <rhoeb21@gmail.com>
2024-05-19 18:04:45 +0900

    [feat] create Book domain

---
+++ src/main/java/com/group/libraryapp/domain/book/Book.java
@@ -0,0 +1,14 @@
+package com.group.libraryapp.domain.book;
+
+import javax.persistence.*;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+    @Column(nullable = false)
+    private String name;
+}
